5200b7f9be08633fa1f2371f32a6b38ad9402be5
etienne-orchis <etienne.razafindramiarina@orchisintl.com>
2025-02-05 16:50:52 +0300

    let's use this parameter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,6 @@ import { AsyncCustomValidatorComponent } from './components/reactive-form/async-
 
 export const routes: Routes = [
   { path: '', redirectTo: '/reactive-form/form-group', pathMatch: 'full' },
-  { path: 'reactive-form/form-group', component: FormGroupComponent },
-  { path: 'reactive-form/async-custom-group', component: AsyncCustomValidatorComponent },
+  { path: 'reactive-form/form-group', component: FormGroupComponent , pathMatch: 'full' },
+  { path: 'reactive-form/async-custom-group', component: AsyncCustomValidatorComponent , pathMatch: 'full'  },
 ];
