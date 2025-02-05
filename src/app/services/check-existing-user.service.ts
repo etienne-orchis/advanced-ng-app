41ce54980222614b4d1789b85e191b7ffaf3cca1
etienne-orchis <etienne.razafindramiarina@orchisintl.com>
2025-02-05 16:20:02 +0300

    implementing the service class

--- src/app/services/check-existing-user.service.ts
+++ src/app/services/check-existing-user.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
+export interface ICheckExistingUser {
+  isExistingUser: boolean;
+}
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CheckExistingUserService {
+  constructor(private readonly http: HttpClient) {}
 
-  constructor() { }
+  checkIfUserExists(username: string): Observable<ICheckExistingUser> {
+    return this.http.get<ICheckExistingUser>(
+      `http://localhost:3000/api/user/${username}`
+    );
+  }
 }
