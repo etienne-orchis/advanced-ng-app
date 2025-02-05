43397f4d2b81fb887140db009709863cf060bf7d
etienne-orchis <etienne.razafindramiarina@orchisintl.com>
2025-02-05 11:20:39 +0300

    the form-group component , the right one

---
+++ src/app/reactive-form/form-group/form-group.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-group',
+  imports: [],
+  templateUrl: './form-group.component.html',
+  styleUrl: './form-group.component.scss'
+})
+export class FormGroupComponent {
+
+}
