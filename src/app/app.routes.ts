2c2d59d121b174bc301c04b5c89c85fb89d15bee
etienne-orchis <etienne.razafindramiarina@orchisintl.com>
2025-02-05 14:08:41 +0300

    the application routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { FormGroupComponent } from './reactive-form/form-group/form-group.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/reactive-form/form-group', pathMatch: 'full' },
+  { path: 'reactive-form/form-group', component: FormGroupComponent },
+];
