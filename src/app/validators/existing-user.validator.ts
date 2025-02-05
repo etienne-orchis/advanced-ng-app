874a05810d11b96ebc47b79d7f2cd5b9746bd05b
etienne-orchis <etienne.razafindramiarina@orchisintl.com>
2025-02-05 16:39:07 +0300

    the custom validator

---
+++ src/app/validators/existing-user.validator.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {
+  AbstractControl,
+  AsyncValidatorFn,
+  ValidationErrors,
+} from '@angular/forms';
+import { Observable, catchError, map, of } from 'rxjs';
+import {
+  CheckExistingUserService,
+  ICheckExistingUser,
+} from '../services/check-existing-user.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ExistingUserValidator {
+  constructor(private readonly usersService: CheckExistingUserService) {}
+
+  userExistsValidator(): AsyncValidatorFn {
+    return (control: AbstractControl): Observable<ValidationErrors | null> => {
+      if (!control.value) {
+        return of(null);
+        // Return null if username field is empty
+      }
+
+      return this.usersService.checkIfUserExists(control.value).pipe(
+        map((data: ICheckExistingUser) =>
+          data.isExistingUser ? { isExistingUser: true } : null
+        ),
+        catchError(() => of(null))
+        // Return null if any error occurs
+      );
+    };
+  }
+}
