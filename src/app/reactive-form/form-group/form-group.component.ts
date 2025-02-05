9932f367f8260b8bc844558aeb645e141a6e76e9
etienne-orchis <etienne.razafindramiarina@orchisintl.com>
2025-02-05 14:19:29 +0300

    creating the form group object

--- src/app/reactive-form/form-group/form-group.component.ts
+++ src/app/reactive-form/form-group/form-group.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-group',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form-group.component.html',
-  styleUrl: './form-group.component.scss'
+  styleUrl: './form-group.component.scss',
 })
 export class FormGroupComponent {
+  signUpForm!: FormGroup;
 
+  constructor(private readonly fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.signUpForm = this.fb.group({
+      // Form Group
+      username: [''], // Form Control
+      password: [''], // Form Control
+    });
+  }
+
+  handleSubmit() {
+    const formValue = this.signUpForm.value;
+
+    console.log('formValue :>> ', formValue);
+  }
 }
