3dd354b7e1e4deae28632e05c6ef1475a1e3e023
etienne-orchis <etienne.razafindramiarina@orchisintl.com>
2025-02-06 09:44:30 +0300

    the full front implementation of the nested form component

--- src/app/components/reactive-form/nested-form/nested-form.component.ts
+++ src/app/components/reactive-form/nested-form/nested-form.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-nested-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './nested-form.component.html',
-  styleUrl: './nested-form.component.scss'
+  styleUrl: './nested-form.component.scss',
 })
 export class NestedFormComponent {
+  myForm!: FormGroup;
 
+  constructor(private readonly fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.myForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      address: this.fb.group({
+        street: ['', Validators.required],
+        city: ['', Validators.required],
+        country: ['', Validators.required],
+      }),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.myForm.value);
+  }
 }
