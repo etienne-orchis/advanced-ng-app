0640b404c9477bd2a57855718d347ec747022c2a
etienne-orchis <etienne.razafindramiarina@orchisintl.com>
2025-02-06 11:45:07 +0300

    adding the route to the testing the custom form control

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { FormGroupComponent } from './components/reactive-form/form-group/form-group.component';
 import { AsyncCustomValidatorComponent } from './components/reactive-form/async-custom-validator/async-custom-validator.component';
 import { NestedFormComponent } from './components/reactive-form/nested-form/nested-form.component';
+import { TestingCustomInputComponent } from './components/testing-custom-input/testing-custom-input.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/reactive-form/form-group', pathMatch: 'full' },
   { path: 'reactive-form/form-group', component: FormGroupComponent , pathMatch: 'full' },
   { path: 'reactive-form/async-custom-group', component: AsyncCustomValidatorComponent , pathMatch: 'full'  },
   { path: 'reactive-form/nested-form', component: NestedFormComponent , pathMatch: 'full'  },
+  { path: 'reactive-form/custom-form-control', component: TestingCustomInputComponent , pathMatch: 'full'  },
 ];
