9f7620014ce725e0cfae1b21276e9677d8a1caf3
etienne-orchis <etienne.razafindramiarina@orchisintl.com>
2025-02-06 09:37:19 +0300

    adding the nested form component to the routinf

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { FormGroupComponent } from './components/reactive-form/form-group/form-group.component';
 import { AsyncCustomValidatorComponent } from './components/reactive-form/async-custom-validator/async-custom-validator.component';
+import { NestedFormComponent } from './components/reactive-form/nested-form/nested-form.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/reactive-form/form-group', pathMatch: 'full' },
   { path: 'reactive-form/form-group', component: FormGroupComponent , pathMatch: 'full' },
   { path: 'reactive-form/async-custom-group', component: AsyncCustomValidatorComponent , pathMatch: 'full'  },
+  { path: 'reactive-form/nested-form', component: NestedFormComponent , pathMatch: 'full'  },
 ];
