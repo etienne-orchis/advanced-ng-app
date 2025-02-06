cc20b133e4f7f00d963a23605f9f4d1fcffaee85
etienne-orchis <etienne.razafindramiarina@orchisintl.com>
2025-02-06 09:34:50 +0300

    the nested form component

---
+++ src/app/components/reactive-form/nested-form/nested-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nested-form',
+  imports: [],
+  templateUrl: './nested-form.component.html',
+  styleUrl: './nested-form.component.scss'
+})
+export class NestedFormComponent {
+
+}
