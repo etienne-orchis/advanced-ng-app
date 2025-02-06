359b8d6b71c9543768ddde2d9e25710f6c308505
etienne-orchis <etienne.razafindramiarina@orchisintl.com>
2025-02-06 11:43:28 +0300

    implementing the full front of the custom form control

--- src/app/components/custom-form-control/custom-form-control.component.ts
+++ src/app/components/custom-form-control/custom-form-control.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ControlValueAccessor, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-custom-form-control',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './custom-form-control.component.html',
-  styleUrl: './custom-form-control.component.scss'
+  styleUrl: './custom-form-control.component.scss',
 })
-export class CustomFormControlComponent {
+export class CustomFormControlComponent implements ControlValueAccessor {
+  @Input() label: string = ''; // Label input
+  value: string = ''; // Internal value of the form control
+  // The function to call when the value changes
+  onChange: (value: any) => void = () => {};
 
+  // The function to call when the form is touched
+  onTouched: () => void = () => {};
+
+  constructor() {}
+
+
+  // Write the value to the input (from the form model)
+  writeValue(value: any): void {
+    if (value !== undefined) {
+      this.value = value;
+    }
+  }
+
+  // Register a function to call when the value changes
+  registerOnChange(fn: (value: any) => void): void {
+    this.onChange = fn;
+  }
+
+  // Register a function to call when the input is touched
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  // Handle the change event in the input
+  onInputChange() {
+    this.onChange(this.value); // Notify Angular forms of the change
+  }
+
+  // Handle the blur event (touched state)
+  onBlur() {
+    this.onTouched(); // Notify Angular forms that the control was touched
+  }
 }
