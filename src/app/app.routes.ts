878204615b9d7bf6feed5a9f26863331929fd44e
etienne-orchis <etienne.razafindramiarina@orchisintl.com>
2025-02-06 14:10:40 +0300

    adding the route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,33 @@ import { FormGroupComponent } from './components/reactive-form/form-group/form-g
 import { AsyncCustomValidatorComponent } from './components/reactive-form/async-custom-validator/async-custom-validator.component';
 import { NestedFormComponent } from './components/reactive-form/nested-form/nested-form.component';
 import { TestingCustomInputComponent } from './components/testing-custom-input/testing-custom-input.component';
+import { SingleSlotComponent } from './components/content-projection/single-slot/single-slot.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/reactive-form/form-group', pathMatch: 'full' },
-  { path: 'reactive-form/form-group', component: FormGroupComponent , pathMatch: 'full' },
-  { path: 'reactive-form/async-custom-group', component: AsyncCustomValidatorComponent , pathMatch: 'full'  },
-  { path: 'reactive-form/nested-form', component: NestedFormComponent , pathMatch: 'full'  },
-  { path: 'custom-form-control', component: TestingCustomInputComponent , pathMatch: 'full'  },
+  {
+    path: 'reactive-form/form-group',
+    component: FormGroupComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'reactive-form/async-custom-group',
+    component: AsyncCustomValidatorComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'reactive-form/nested-form',
+    component: NestedFormComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'custom-form-control',
+    component: TestingCustomInputComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'content-projection/single-slot',
+    component: SingleSlotComponent,
+    pathMatch: 'full',
+  },
 ];
