259c356a7c941929ba1123cb28f6569785d10039
etienne-orchis <etienne.razafindramiarina@orchisintl.com>
2025-02-06 16:37:32 +0300

    wrong component name

--- src/app/components/content-projection/child/multi-slot/multi-slot.component.spec.ts
+++ src/app/components/content-projection/child/multi-slot/multi-slot.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MutliSlotComponent } from './mutli-slot.component';
+import { MultiSlotComponent } from './multi-slot.component';
 
-describe('MutliSlotComponent', () => {
-  let component: MutliSlotComponent;
-  let fixture: ComponentFixture<MutliSlotComponent>;
+describe('MultiSlotComponent', () => {
+  let component: MultiSlotComponent;
+  let fixture: ComponentFixture<MultiSlotComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [MutliSlotComponent]
+      imports: [MultiSlotComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(MutliSlotComponent);
+    fixture = TestBed.createComponent(MultiSlotComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/components/content-projection/child/multi-slot/multi-slot.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-multi-slot',
+  imports: [],
+  templateUrl: './multi-slot.component.html',
+  styleUrl: './multi-slot.component.scss'
+})
+export class MultiSlotComponent {
+
+}
