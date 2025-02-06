3ff92e7062ea69ea15e1b62fac20a3e98bec73c4
etienne-orchis <etienne.razafindramiarina@orchisintl.com>
2025-02-06 11:28:52 +0300

    the custom form control component

---
+++ src/app/components/custom-form-control/custom-form-control.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-form-control',
+  imports: [],
+  templateUrl: './custom-form-control.component.html',
+  styleUrl: './custom-form-control.component.scss'
+})
+export class CustomFormControlComponent {
+
+}
