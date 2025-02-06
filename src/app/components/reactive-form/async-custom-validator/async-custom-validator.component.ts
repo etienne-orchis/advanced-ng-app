3d2163f81b9d7608bde34767260bf7ce2d89d4c7
etienne-orchis <etienne.razafindramiarina@orchisintl.com>
2025-02-06 08:21:05 +0300

    the full implementation of the component

--- src/app/components/reactive-form/async-custom-validator/async-custom-validator.component.ts
+++ src/app/components/reactive-form/async-custom-validator/async-custom-validator.component.ts
@@ -1,16 +1,62 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UsersValidator } from '../../../validators/users.validator';
 
 @Component({
   selector: 'app-async-custom-validator',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './async-custom-validator.component.html',
   styleUrl: './async-custom-validator.component.scss',
 })
 export class AsyncCustomValidatorComponent {
+  signUpForm!: FormGroup;
+
   constructor(
     private readonly fb: FormBuilder,
     private readonly usersValidator: UsersValidator
   ) {}
+
+  ngOnInit(): void {
+    this.signUpForm = this.fb.group({
+      username: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(24),
+        ],
+        [this.usersValidator.userExistsValidator()], // async validator
+      ],
+      password: ['', [Validators.required, Validators.minLength(10)]],
+      age: ['', [Validators.min(12), Validators.pattern('^[0-9]*$')]],
+      rememberMe: [true],
+    });
+  }
+
+  get usernameControl(): AbstractControl {
+    return this.signUpForm.get('username') as AbstractControl;
+  }
+
+  get passwordControl(): AbstractControl {
+    return this.signUpForm.get('password') as AbstractControl;
+  }
+
+  get ageControl(): AbstractControl {
+    return this.signUpForm.get('age') as AbstractControl;
+  }
+
+  get rememberMeControl(): AbstractControl {
+    return this.signUpForm.get('rememberMe') as AbstractControl;
+  }
+
+  handleSubmit() {
+    const formValue = this.signUpForm.value;
+    const isFormValid = this.signUpForm.valid;
+    console.log('isFormValid :>> ', isFormValid);
+
+    if (!isFormValid) {
+      return;
+    }
+    console.log('formValue :>> ', formValue);
+  }
 }
