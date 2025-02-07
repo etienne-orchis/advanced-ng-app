7faeea0c5b3b7becae56dfa18aa32141f6f1c184
etienne-orchis <etienne.razafindramiarina@orchisintl.com>
2025-02-07 13:44:38 +0300

    the ng template component

---
+++ src/app/components/ng-template/ng-template.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-template',
+  imports: [],
+  templateUrl: './ng-template.component.html',
+  styleUrl: './ng-template.component.scss'
+})
+export class NgTemplateComponent {
+
+}
