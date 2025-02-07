e094c47421130b7b6c078e7f7fc3528de8d3ae65
etienne-orchis <etienne.razafindramiarina@orchisintl.com>
2025-02-07 08:41:18 +0300

    the conditional ng template component

---
+++ src/app/components/ng-template/conditional-template/conditional-template.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-conditional-template',
+  imports: [],
+  templateUrl: './conditional-template.component.html',
+  styleUrl: './conditional-template.component.scss'
+})
+export class ConditionalTemplateComponent {
+
+}
